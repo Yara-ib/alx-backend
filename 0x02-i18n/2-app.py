0c950ac72dd40243671c6687aed561722cfe76ef
Yara-ib <yr.ap.23@gmail.com>
2024-09-14 00:55:50 +0300

    Parametrize templates

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -32,7 +32,7 @@ def home_page() -> str:
     Returns:
         str: Hello world as header in html page
     """
-    return render_template("1-index.html")
+    return render_template("2-index.html")
 
 
 if __name__ == "__main__":
