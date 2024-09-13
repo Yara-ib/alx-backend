aa641cab292d0c2cd4603f8f0bbfaab0a942b237
Yara-ib <yr.ap.23@gmail.com>
2024-09-13 23:51:51 +0300

    Basic Flask App

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home_page() -> str:
+    """
+    Basic Flask app
+
+    Returns:
+        str: Hello world as header in html page
+    """
+    return render_template("0-index.html")
