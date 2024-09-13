1371e1713ef7de977291555b1acac585329219d5
Yara-ib <yr.ap.23@gmail.com>
2024-09-14 00:17:58 +0300

    Basic Babel setup

--- 0x02-i18n/1.app.py
+++ 0x02-i18n/1.app.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 """ Basic Babel setup Module """
-
-
 from flask import Flask, render_template
 from flask_babel import Babel
 
@@ -9,18 +7,18 @@ app = Flask(__name__)
 
 
 class Config:
-    """class Config"""
+    """Config class"""
 
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
+    BABEL_DEFAULT_LOCALE = "en"
 
 
-app.config.from_object(Config)
 babel = Babel(app)
+app.config.from_object(Config)
 
 
-@app.route("/")
+@app.route("/", strict_slashes=False)
 def home_page() -> str:
     """
     Basic Flask app
@@ -29,3 +27,7 @@ def home_page() -> str:
         str: Hello world as header in html page
     """
     return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
