f0620332ff76d2d944a659e5f8db94b9f785d2a4
Yara-ib <yr.ap.23@gmail.com>
2024-09-25 23:09:49 +0300

    Node Redis Client

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,18 +1,3 @@
-// import { createClient } from 'redis';
-
-// const client = await createClient();
-
-// client.on('error', (err) =>
-//   console.error(`Redis client not connected to the server: ${err}`)
-// );
-// console.log('Redis client connected to the server').connect();
-
-// const client = await createClient()
-//   .on('error', (err) =>
-//     console.error(`Redis client not connected to the server: ${err}`)
-//   )
-//   .connect(() => console.log('Redis client connected to the server'));
-
 import { createClient } from 'redis';
 
 async function connectRedis() {
