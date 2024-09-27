a1840e5b7604958821717999ac4573aea52d1dd0
Yara-ib <yr.ap.23@gmail.com>
2024-09-28 02:33:59 +0300

    Jobs using Kue

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -1,8 +1,20 @@
-import kue from 'kue';
+import { createQueue } from 'kue';
 
-const queue = kue.createQueue();
-
-const push_notification_code = queue.create({
-    phoneNumber: string,
-    message: string,
+const queue = createQueue();
+const job = queue
+  .create('push_notification_code', {
+    phoneNumber: 4153518780,
+    message: 'This is the code to verify your account',
   })
+  .save((err) => {
+    if (err) console.log('Notification job failed');
+    console.log(`Notification job created: ${job.id}`);
+  });
+
+job
+  .on('complete', () => {
+    console.log('Notification job completed');
+  })
+  .on('failed', () => {
+    console.log('Notification job failed');
+  });
